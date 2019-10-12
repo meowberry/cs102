b00c89363ebfd5b66a89548a7a0bc0728cb1c499
anyamatrokhina <ann.matrokhina@gmail.com>
2019-10-13 02:10:40 +0300

    Реализованы функции multiplicative_inverse() и generate_keypair()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,32 +1,56 @@
-def encrypt_caesar(plaintext):
+def encrypt_caesar(plaintext, shift):
     """
     Encrypts plaintext using a Caesar cipher.
-
-    >>> encrypt_caesar("PYTHON")
+    >>> encrypt_caesar("PYTHON", 3)
     'SBWKRQ'
-    >>> encrypt_caesar("python")
+    >>> encrypt_caesar("python", 3)
     'sbwkrq'
-    >>> encrypt_caesar("Python3.6")
+    >>> encrypt_caesar("Python3.6", 3)
     'Sbwkrq3.6'
-    >>> encrypt_caesar("")
+    >>> encrypt_caesar("", 3)
     ''
     """
-    # PUT YOUR CODE HERE
+    ciphertext = ""
+    for char in plaintext:
+        if "A" <= char <= "z":
+            symbol = ord(char) + shift % 26
+
+            if "A" <= char <= "Z" < chr(symbol):
+                symbol -= 26
+
+            elif chr(symbol) > "z":
+                symbol -= 26
+
+            char = chr(symbol)
+        ciphertext += char
+
     return ciphertext
 
 
-def decrypt_caesar(ciphertext):
+def decrypt_caesar(ciphertext, shift):
     """
-    Decrypts a ciphertext using a Caesar cipher.
+     Decrypts a ciphertext using a Caesar cipher.
+     >>> decrypt_caesar("SBWKRQ", 3)
+     'PYTHON'
+     >>> decrypt_caesar("sbwkrq", 3)
+     'python'
+     >>> decrypt_caesar("Sbwkrq3.6", 3)
+     'Python3.6'
+     >>> decrypt_caesar("", 3)
+     ''
+     """
+    plaintext = ""
+    for char in ciphertext:
+        if "A" <= char <= "z":
+            enc = ord(char) - shift % 26
 
-    >>> decrypt_caesar("SBWKRQ")
-    'PYTHON'
-    >>> decrypt_caesar("sbwkrq")
-    'python'
-    >>> decrypt_caesar("Sbwkrq3.6")
-    'Python3.6'
-    >>> decrypt_caesar("")
-    ''
-    """
-    # PUT YOUR CODE HERE
+            if enc < ord("A"):
+                enc += 26
+
+            if "a" <= char <= "z" and enc < ord("a"):
+                enc += 26
+
+            char = chr(enc)
+        plaintext += char
+
     return plaintext
